3fd7f5e8e7e598134ff68e053fda3d31bcd0e1b3
HydraCoding <goekay.sahin@gmx.net>
2023-06-28 12:52:08 +0200

    safe status befor relase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,12 @@
 export const environment = {
   firebase: {
-    projectId: 'ringof-4e427',
-    appId: '1:384138021403:web:be0764b956e7025f0306be',
-    storageBucket: 'ringof-4e427.appspot.com',
     apiKey: 'AIzaSyA3B4cDOaFaw53wji-lY4tMtrmG0IoFufs',
     authDomain: 'ringof-4e427.firebaseapp.com',
+    databaseURL:
+      'https://ringof-4e427-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'ringof-4e427',
+    storageBucket: 'ringof-4e427.appspot.com',
     messagingSenderId: '384138021403',
+    appId: '1:384138021403:web:be0764b956e7025f0306be',
   },
 };
